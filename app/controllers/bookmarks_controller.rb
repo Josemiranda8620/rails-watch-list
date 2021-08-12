95893ea172c66820a594d9e9621254b3dedf6707
Jose Miranda <josemiranda8620@hotmail.com>
2021-08-13 00:58:06 +0900

    commit

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,36 @@
+class BookmarksController < ApplicationController
+
+    def new
+        @list = List.find(params[:list_id])
+        @bookmark = Bookmark.new 
+        # @movies = !@list.movies.order(title: : asc)
+    end
+
+    def create
+        @list = List.find(params[:list_id])
+        @movie = Movie.find(params[:bookmark][:movie_id])
+
+        @bookmark = Bookmark.new(bookmark_params)
+        @bookmark.list = @list
+        @bookmark.movie = @movie
+
+        if @bookmark.save
+            redirect_to list_path(@list)
+        else
+            render :new  
+        end      
+    end
+
+    def destroy
+        @bookmark = Bookmark.find(params[:id])
+        @bookmark.destroy
+        redirect_to list_path(@list)
+    end
+
+    private
+
+    def bookmark_params
+        params.require(:bookmark).permit(:movie_id, :comment)
+    end
+end
+
